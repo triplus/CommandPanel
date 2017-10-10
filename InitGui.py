6efeb45b95327e921caf689bd3efb75ad713eb7a
triplus <triplus@users.noreply.github.com>
2017-10-11 01:47:23 +0200

    Basic widget and license

---
+++ InitGui.py
@@ -0,0 +1,25 @@
+# Command panel for FreeCAD
+# Copyright (C) 2015, 2016 (as part of TabBar) triplus @ FreeCAD
+# Copyright (C) 2017 triplus @ FreeCAD
+#
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+"""Command panel for FreeCAD."""
+
+p = FreeCAD.ParamGet("User parameter:BaseApp/CommandPanel")
+
+if p.GetBool("Enabled", 1):
+    import Command_Panel_Gui
