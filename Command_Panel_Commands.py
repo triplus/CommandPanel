b4c9eab37740df7ce1b68fd0563c8f790bf6eaf1
triplus <triplus@users.noreply.github.com>
2018-01-05 17:38:29 +0100

    Expand or collapse menu
    - Expand or collapse menu
    - Persistent button and menu default action
    - Set default menu action only if action was triggered from the menu
    - Rework existing artwork to omit Qt SVG support related messages
    - Artwork

--- Command_Panel_Commands.py
+++ Command_Panel_Commands.py
@@ -1,6 +1,6 @@
 # Command panel for FreeCAD
 # Copyright (C) 2015, 2016 (as part of TabBar) triplus @ FreeCAD
-# Copyright (C) 2017 triplus @ FreeCAD
+# Copyright (C) 2017, 2018 triplus @ FreeCAD
 #
 #
 # This library is free software; you can redistribute it and/or
@@ -19,21 +19,46 @@
 
 """Command panel for FreeCAD - Commands."""
 
+import os
 import FreeCAD as App
 import FreeCADGui as Gui
 from PySide import QtGui
+from PySide import QtCore
+import Command_Panel_Gui as cpg
 import Command_Panel_Common as cpc
 import Command_Panel_Event_Filter as cpef
 
+global currentMenu
+currentMenu = None
+
+menuList = []
+buttonList = []
 mw = Gui.getMainWindow()
+mapperShow = QtCore.QSignalMapper()
+mapperExpandCollapse = QtCore.QSignalMapper()
 p = App.ParamGet("User parameter:BaseApp/CommandPanel")
+path = os.path.dirname(__file__) + "/Resources/icons/"
+
+
+class CommandButton(QtGui.QToolButton):
+    """Clear currentMenu on button press event."""
+    def __init__(self, parent=None):
+        super(CommandButton, self).__init__()
+
+    def mousePressEvent(self, event):
+        """Press event."""
+        global currentMenu
+        currentMenu = None
+        QtGui.QToolButton.mousePressEvent(self, event)
 
 
 def workbenchButtons(workbench):
     """Create workbench buttons from command names."""
+    clearList(menuList)
+    clearList(buttonList)
+
     g = None
     uid = None
-    buttons = []
     actions = cpc.actionList()
     base = p.GetGroup("User").GetGroup(workbench)
     cpc.defaultGroup(base)
@@ -44,13 +69,28 @@ def workbenchButtons(workbench):
         commands = g.GetString("commands")
         if commands:
             commands = commands.split(",")
+            commands = menuCommands(base, commands)
         else:
             commands = []
         for cmd in commands:
-            btn = QtGui.QToolButton()
+            btn = CommandButton()
             btn.setAutoRaise(True)
             btn.installEventFilter((cpef.InstallEvent(btn)))
-            if cmd == "CP_Separator":
+            if cmd.startswith("CP_Collapse_"):
+                a = QtGui.QAction(btn)
+                try:
+                    gUid = cmd.split("CP_Collapse_", 1)[1]
+                except IndexError:
+                    gUid = "No_UID"
+                data = ",".join([workbench, gUid, str(0)])
+                a.setData(data)
+                a.setText("Collapse")
+                a.setIcon(QtGui.QIcon(path + "CommandPanel_Collapse.svg"))
+                a.setToolTip("Collapse menu")
+                btn.setDefaultAction(a)
+                mapperExpandCollapse.setMapping(btn, data)
+                btn.clicked.connect(mapperExpandCollapse.map)
+            elif cmd == "CP_Separator":
                 btn.setEnabled(False)
                 btn.setObjectName("CP_Separator")
             elif cmd == "CP_Spacer":
@@ -58,16 +98,18 @@ def workbenchButtons(workbench):
                 btn.setObjectName("CP_Spacer")
             elif cmd == "CP_Menu":
                 menu = QtGui.QMenu()
-                btn.setEnabled(False)
                 btn.setMenu(menu)
                 btn.setIcon(QtGui.QIcon(":/icons/freecad"))
+                # Themes support
+                btn.setObjectName("qt_toolbutton_menubutton")
                 btn.setPopupMode(QtGui.QToolButton
                                  .ToolButtonPopupMode.MenuButtonPopup)
                 btn.setToolTip("Empty menu")
             elif cmd.startswith("CP_Menu_"):
-                menu = menuButton(base, cmd, btn, actions)
+                menu = menuButton(workbench, base, cmd, btn, actions)
                 btn.setMenu(menu)
-                btn.triggered.connect(btn.setDefaultAction)
+                # Theme support
+                btn.setObjectName("qt_toolbutton_menubutton")
                 btn.setPopupMode(QtGui.QToolButton
                                  .ToolButtonPopupMode.MenuButtonPopup)
             elif cmd in actions:
@@ -78,22 +120,27 @@ def workbenchButtons(workbench):
                 btn.setEnabled(False)
                 btn.setToolTip("Command " +
                                cmd +
-                               " is not currently available")
+                               " is currently not available")
                 btn.setIcon(QtGui.QIcon(":/icons/freecad"))
-            buttons.append(btn)
+            buttonList.append(btn)
 
-    return buttons
+    for m in menuList:
+        m.triggered.connect(onMenuTriggered)
+
+    return buttonList
 
 
-def menuButton(base, cmd, btn, actions):
+def menuButton(workbench, base, cmd, btn, actions):
     """Create menu for menu button from command names."""
     menu = QtGui.QMenu(mw)
+    menuList.append(menu)
     try:
         uid = cmd.split("CP_Menu_", 1)[1]
     except IndexError:
-        uid = None
+        uid = "No_UID"
+    menu.setObjectName(uid)
     g = cpc.findGroup(base, uid)
-    if g:
+    if g and uid != "No_UID":
         commands = g.GetString("commands")
         if commands:
             commands = commands.split(",")
@@ -111,11 +158,169 @@ def menuButton(base, cmd, btn, actions):
                 a.setEnabled(False)
                 a.setText(cmd)
                 a.setIcon(QtGui.QIcon(":/icons/freecad"))
-                a.setToolTip("Command " + cmd + " is not currently available")
+                a.setToolTip("Command " + cmd + " is currently not available")
                 menu.addAction(a)
-            try:
-                btn.setDefaultAction(menu.actions()[0])
-            except IndexError:
-                pass
+        # Set default action
+        try:
+            btn.setDefaultAction(menu.actions()[0])
+            menu.setDefaultAction(menu.actions()[0])
+        except IndexError:
+            pass
+
+        default = g.GetString("Default")
+        for a in menu.actions():
+            if a.objectName() == default:
+                btn.setDefaultAction(a)
+                menu.setDefaultAction(a)
+
+        # Add expand action
+        data = ",".join([workbench, uid, str(1)])
+
+        e = QtGui.QAction(menu)
+        e.setText("Expand")
+        e.setIcon(QtGui.QIcon(path + "CommandPanel_Expand.svg"))
+        e.setToolTip("Expand menu")
+        e.setData(data)
+
+        menu.addSeparator()
+        menu.addAction(e)
+
+        mapperExpandCollapse.setMapping(e, data)
+        e.triggered.connect(mapperExpandCollapse.map)
+
+        mapperShow.setMapping(menu, data)
+        menu.aboutToShow.connect(mapperShow.map)
 
     return menu
+
+
+def menuCommands(base, commands):
+    """Add command names for expanded menu."""
+    names = []
+    for cmd in commands:
+        if cmd.startswith("CP_Menu_"):
+            try:
+                uid = cmd.split("CP_Menu_", 1)[1]
+            except IndexError:
+                uid = "No_UID"
+            g = cpc.findGroup(base, uid)
+            if g and uid != "No_UID":
+                expand = g.GetBool("Expand", 0)
+            else:
+                expand = 0
+            if expand:
+                gE = g.GetString("commands")
+                if gE:
+                    gE = gE.split(",")
+                else:
+                    gE = []
+                for e in gE:
+                    if e.startswith("CP_Menu"):
+                        pass
+                    else:
+                        names.append(e)
+                # Move spacer after collapse button
+                try:
+                    last = names.pop()
+                except IndexError:
+                    last = None
+                if last == "CP_Spacer":
+                    names.append("CP_Collapse_" + uid)
+                    names.append(last)
+                elif last:
+                    names.append(last)
+                    names.append("CP_Collapse_" + uid)
+                else:
+                    names.append("CP_Collapse_" + uid)
+            else:
+                names.append(cmd)
+        else:
+            names.append(cmd)
+
+    return names
+
+
+def clearList(l):
+    """Empty list and delete the items."""
+    try:
+        item = l.pop()
+    except IndexError:
+        item = None
+    while item:
+        item.deleteLater()
+        try:
+            item = l.pop()
+        except IndexError:
+            item = None
+
+
+def onMenuShow(n):
+    """Set current menu name on menu aboutToShow"""
+    global currentMenu
+    currentMenu = n
+
+
+def onMenuTriggered(a):
+    """Set default menu action on menu triggered."""
+    try:
+        data = currentMenu.split(",")
+    except AttributeError:
+        data = []
+    try:
+        wb = data[0]
+        uid = data[1]
+    except IndexError:
+        wb = None
+        uid = None
+    if wb:
+        base = p.GetGroup("User").GetGroup(wb)
+    else:
+        base = None
+    if base and uid:
+        g = cpc.findGroup(base, uid)
+    else:
+        g = None
+
+    for m in menuList:
+        if m.objectName() == uid:
+            m.setDefaultAction(a)
+            for b in buttonList:
+                if b.menu() == m:
+                    b.setDefaultAction(a)
+            name = a.objectName()
+            if g and name:
+                g.SetString("Default", name)
+
+
+def onMenuExpandCollapse(s):
+    """Set expand or collapse menu parameter."""
+    g = None
+    base = None
+    try:
+        data = s.split(",")
+    except AttributeError:
+        data = []
+    try:
+        wb = data[0]
+        uid = data[1]
+        expand = data[2]
+    except IndexError:
+        wb = None
+        uid = None
+        expand = None
+    if wb:
+        base = p.GetGroup("User").GetGroup(wb)
+    if base and uid:
+        g = cpc.findGroup(base, uid)
+    if g and expand == "1":
+        g.SetBool("Expand", 1)
+    elif g:
+        g.SetBool("Expand", 0)
+    else:
+        pass
+
+    cpg.onWorkbench()
+
+
+mapperShow.mapped[str].connect(onMenuShow)
+mapperExpandCollapse.mapped[str].connect(onMenuExpandCollapse)
