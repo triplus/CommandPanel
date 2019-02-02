75e63beed871a37954f967de85e903133415328e
triplus <triplus@users.noreply.github.com>
2019-02-02 17:07:07 +0100

    Set Accessories menu visible (macOS)

--- CommandPanelGui.py
+++ CommandPanelGui.py
@@ -129,6 +129,7 @@ def accessoriesMenu():
                 toolsMenu = mb.findChild(QtGui.QMenu, "&Tools")
                 if toolsMenu:
                     toolsMenu.addAction(action)
+                    action.setVisible(True)
 
             addMenu()
             mw.workbenchActivated.connect(addMenu)
